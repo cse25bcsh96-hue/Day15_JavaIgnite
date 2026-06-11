79f402696c1b0fb3eca18115320b22ac2b289566
rite2harini <rite2harini@gmail.com>
2026-06-11 13:37:25 +0530

    Create TicketBookingSystem.java

---
+++ Multithreading/TicketBookingSystem.java
@@ -0,0 +1,81 @@
+/*
+Ticket Booking System
+
+A movie theater has:
+
+Total Seats = 5
+
+Create:
+
+Multiple booking threads
+Requirements
+Each thread tries to book seats.
+If seats are available, booking succeeds.
+Otherwise print:
+Booking Failed
+
+Concepts Tested
+Threads
+Shared Resource
+Synchronized Method
+
+Instructions - 
+Create class Theater
+
+    Variable:
+        totalSeats = 5
+
+    synchronized method bookSeat(int seats)
+
+        If totalSeats >= seats
+
+            Print:
+                ThreadName + " Booking Successful"
+
+            totalSeats = totalSeats - seats
+
+            Print remaining seats
+
+        Else
+
+            Print:
+                ThreadName + " Booking Failed"
+
+
+Create class BookingThread extends Thread
+
+    Theater theater
+    int seatsRequired
+
+    Constructor receives:
+        theater
+        seatsRequired
+
+    run()
+
+        theater.bookSeat(seatsRequired)
+
+
+Main Method
+
+    Create Theater object
+
+    Create Thread1 -> wants 2 seats
+    Create Thread2 -> wants 2 seats
+    Create Thread3 -> wants 2 seats
+
+    Start Thread1
+    Start Thread2
+    Start Thread3
+
+
+Possible Output
+
+    Thread1 Booking Successful
+    Remaining Seats = 3
+
+    Thread2 Booking Successful
+    Remaining Seats = 1
+
+    Thread3 Booking Failed
+*/
